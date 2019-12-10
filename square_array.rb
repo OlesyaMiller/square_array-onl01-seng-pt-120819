3c873d40e6cd1f90dfb34e642eac9c771ad3002f
Olesya Miller <olesyam.miller@gmail.com>
2019-12-10 21:59:53 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,7 +1,9 @@
 def square_array(array)
   # your code here
   
-  new_array = []
-  array.each { |element| new_array << element * element }
-  new_array
+  # new_array = []
+  # array.each { |element| new_array << element * element }
+  # new_array
+  
+  array.collect { |element| element ** 2 }
 end
