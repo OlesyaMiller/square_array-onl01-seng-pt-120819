e56b1f3b2dc3998a6012a2e24b9015b477300648
Olesya Miller <olesyam.miller@gmail.com>
2019-12-10 22:02:41 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,8 +1,8 @@
 def square_array(array)
 
-  # new_array = []
-  # array.each { |element| new_array << element * element }
-  # new_array
+  new_array = []
+  array.each { |element| new_array << element * element }
+  new_array
   
-  array.collect { |element| element ** 2 }
+  #array.collect { |element| element ** 2 }
 end
